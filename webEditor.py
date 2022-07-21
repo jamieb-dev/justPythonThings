279965d4af5e15445e807a4a3423c674f5c33884
J-Boii <106492855+J-Boii@users.noreply.github.com>
2022-07-21 14:11:19 +0100

    Test web editor functionality

---
+++ webEditor.py
@@ -0,0 +1,12 @@
+def greet():
+    name = input("Please enter your name: ")
+    print("It's loveley to meet you " + str(name))
+
+def main():
+    print("Loveley to meet you " + str(getName))
+
+def getName():
+    return input("Please enter your name: ")
+
+
+main()
